0af9cb9d42fced608f3a02f9f8b9adf57f9c54d5
Giovanni <giovannicruzvinhedo@gmail.com>
2025-07-16 17:00:07 -0300

    Update layout metadata with Open Graph and Twitter card details

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import Head from "next/head"
 
-
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -15,11 +14,15 @@ export const metadata: Metadata = {
   authors: [{ name: "Jaime Cruz" }],
   robots: "index, follow",
   openGraph: {
+    images: [
+      "https://opengraph.b-cdn.net/production/images/933998ff-5ff7-4fd1-8589-c8a94956fcbf.jpg?token=M9VAgcewcg71EAIm_5aKB8xbnzY0Q_AC_0QGOgr6yuE&height=630&width=1200&expires=33288695844",
+    ],
     title: "Jaime Cruz - Vitória da Justiça",
     description:
       "A verdade triunfa: A justiça e a luz da vitória em Jaime Cruz",
     type: "website",
     locale: "pt_BR",
+    url: "https://jaimecruz.vercel.app/",
   },
   twitter: {
     card: "summary_large_image",
@@ -27,6 +30,7 @@ export const metadata: Metadata = {
     description:
       "A verdade triunfa: A justiça e a luz da vitória em Jaime Cruz",
   },
+
   viewport: "width=device-width, initial-scale=1",
   themeColor: "#000000",
   colorScheme: "dark",
